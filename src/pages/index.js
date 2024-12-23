35d633e131f2921a9b55e3fc1c76859c7db420a8
OkanLimitless <okangalatasaray001@gmail.com>
2024-12-23 19:20:43 +0100

    Add main app pages

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Generator from '@/components/Generator';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <Generator />
+    </div>
+  );
+}
