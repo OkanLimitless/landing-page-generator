c6a08b6be9595ed7dfd056f9db9580e36309b398
OkanLimitless <okangalatasaray001@gmail.com>
2024-12-24 13:16:19 +0100

    Simplify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['*'],
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.join(__dirname, 'src'),
-    };
-    return config;
-  },
-  experimental: {
-    appDir: false,
-  },
+  swcMinify: true,
 }
 
 module.exports = nextConfig
